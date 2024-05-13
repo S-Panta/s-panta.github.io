997138ff5413e8ded00b0c9e64c4eb495c116f9b
Sabin Panta <64484313+S-Panta@users.noreply.github.com>
2024-05-13 20:27:37 +0545

    updated personal project file (#7)

--- script.js
+++
@@ -1,6 +0,0 @@
-const sidebar = document.querySelector("[data-sidebar]");
-const sidebarBtn = document.querySelector("[data-sidebar-btn]");
-
-sidebarBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("active")
-})
